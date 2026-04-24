ba2fcadcdf98b723d7fb8ba3bcc4144f49beeed3
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2026-04-24 16:02:10 -0300

    0206 Ambiente Curso

---
+++ 02 - React Para Iniciantes/Ambiente Curso/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+         testes
+
+         <button>Adicionar Produto</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
