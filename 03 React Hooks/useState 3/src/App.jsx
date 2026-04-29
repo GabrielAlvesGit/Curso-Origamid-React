71b12e1f2f8608537f8cf6ebe72b641cf694660f
GabrielAlvesGit <gabrielalves.-100@hotmail.com>
2026-04-29 11:23:17 -0300

    0301 useState 3

---
+++ 03 React Hooks/useState 3/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Produto from './Produto';
+
+function App() {
+  const [dados, setDados] = useState(null);
+  const [carregando, setCarregando] = useState(null);
+
+  async function handleClick(event) {
+    setCarregando(true)
+    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
+    const json = await response.json()
+    setDados(json)
+    setCarregando(false)
+    console.log(json)
+  }
+
+    return (
+      <>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>notebook</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>smartphone</button>
+        <button style={{margin: '.5rem'}} onClick={handleClick}>tablet</button>
+        {carregando && <p>Carregando...</p>}
+        {dados && <Produto dados={dados}/>}
+      </>
+    );
+}
+
+export default App
